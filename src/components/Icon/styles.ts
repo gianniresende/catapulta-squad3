cf20d5d844cb63d50896320191b7c1a3e6947865
gianniresende <gianniresende@gmail.com>
2021-11-30 19:07:25 -0300

    feat: remove eslint warns

--- src/components/Icon/styles.ts
+++ src/components/Icon/styles.ts
@@ -1,8 +1,5 @@
 import styled from 'styled-components/native';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {IconProps} from './types';
-
-type ImageProp = Required<Pick<IconProps, 'size'>>;
 
 export const Container = styled.Image<Partial<IconProps>>`
   min-width: ${({size}) => size}px;
--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {Props} from './types';
 import {Container} from './styles';
 
 const Text = ({children, color, typography, ...rest}: Props) => {
   return (
-    // eslint-disable-next-line prettier/prettier
     <Container color={color || 'primary'}
       typography={typography || 'body1'}
       {...rest}>
